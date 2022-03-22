c6ea856d5e88b3d478529900fddd683c6f7c1175
ShirAmit96 <92683819+ShirAmit96@users.noreply.github.com>
2022-03-22 15:19:46 +0200

    Update backend/app.js
    Co-authored-by: Arnon <ArnonEilat@users.noreply.github.com>

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,6 @@ const app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
