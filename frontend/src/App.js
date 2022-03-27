b1ddbf110ca36e26b5e58b8f7ef22ddf08b2a8f0
shirelch <elchayani@mail.tau.ac.il>
2022-03-27 11:55:08 +0300

    updated frontend app, added a new js comp 'calenderReg'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import {Link} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>this is a test header</h1>
+     
+     <Link to={"./components/calenderReg.js"}>
+       go to registration
+    </Link>
     </div>
   );
 }
---
+++ frontend/src/components/calenderReg.js
@@ -0,0 +1,13 @@
+import './calenderReg.css';
+function calenderReg() {
+  
+    return (
+      <>
+      <div className='header-container'>
+        <h2>When are you comming?</h2>  
+      </div>
+     </>
+    );
+  }
+
+  export default calenderReg;
