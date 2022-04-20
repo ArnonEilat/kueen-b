214865bbb96938bbd3027317e8c72713e774a8e9
yasminsawaid <yasminsawaed4@gmail.com>
2022-04-20 17:52:18 +0300

    update APIcall function

--- frontend/src/APIcall.js
+++ frontend/src/APIcall.js
@@ -1,13 +1,13 @@
 // fetching the GET route from the Express server which matches the GET route from server.js
 const getData = async () => {
-  const response = await fetch('/express_backend');
-  const body = await response.json();
-
   try {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+    if (response.status !== 200) {
+      throw Error(body.message);
+    }
+    return body;
   } catch (e) {
-    throw Error(body.message)
+    throw e;
   }
-  return body;
 };
-
-export default getData;
