3d25c300e4be43b41eeff7b6031ef65ebbb5429c
Liel Mushiev <lielmush@gmail.com>
2022-04-24 16:56:12 +0300

    functional registration button

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 5000;
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+app.listen(port, () => {
+  console.log('Example app listening on port 5000!');
 });
 
 app.get('/validation', (req, res) => {
+  console.log(req.query)
+  let email = req.query.email;
+  console.log('emailll: ' + email);
   if (email.includes('@') && email.includes('.com')) {
-    res.send('Log in successful!');
+    console.log('great')
+    res.send({express:'Log in successful!'});
   } else {
-    res.send('Invalid Email. Please try again.')
+    res.send({express:'Invalid Email. Please try again.'})
   };
 
+});
+
+
+app.on("error", error => {
+  throw new Error(`[app]::ERROR:${error.message}`);
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./index.css";
 import K_Health_logo from "./logoK.svg";
 import img_button from "./calanderIcon.svg";
+import "./App.css";
 
 function App() {
-  const [name, setName] = React.useState("");
-  const [email, setEmail] = React.useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [data, setData] = useState(null);
+
+  const getData = async (email) => {
+    try {
+      const response = await fetch(`/validation?email=${email}`);
+      const body = await response.json();
+      if (response.status !== 200) {
+        throw Error(body.message);
+      }
+      setData(body.express)
+      return body;
+    } catch (e) {
+      throw e;
+    }
+  };
+
+  useEffect(() => {
+    // Call our fetch function below once the component mounts
+    getData()
+      .then((res) => setData(res.express))
+      .catch((err) => console.log(err));
+  }, []);
+
+  // fetch(`http://localhost:5000/validation?email=${email}`)
+  // .then(response => response.json())
+  // .then(data => console.log(data));
 
   return (
     <div className="App">
@@ -29,10 +56,7 @@ function App() {
       <br></br>
       <br></br>
       <br></br>
-      <button
-        onClick={() => console.log(name + " " + email)}
-        id="registerButton"
-      >
+      <button onClick={() => getData(email)} id="registerButton">
         {" "}
         <img src={img_button} className="imgButton" />
         <br></br>
@@ -40,6 +64,7 @@ function App() {
           Register to a day<br></br> in the office
         </p>
       </button>
+      <p>{data}</p>
     </div>
   );
 }
