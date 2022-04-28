5e76fa40f64e8ee81d6ac6b6228e94a0942177af
shirelch <elchayani@mail.tau.ac.il>
2022-04-28 16:22:43 +0300

    solved issues

--- backend/date.js
+++ backend/date.js
@@ -1,21 +1,19 @@
 const mongoose = require('mongoose');
 
-module.exports = function(mongoose){
+module.exports = function (mongoose) {
     const Schema = mongoose.Schema;
 
-const DateSchema = new Schema({
-  
-    Date: {
-        type: String,
-        required: true
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true
-    }
-});
-const model = mongoose.model('Date', DateSchema);
-return model;
+    const DateSchema = new Schema({
+
+        Date: {
+            type: Date(),
+            required: true
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true
+        }
+    });
+    const model = mongoose.model('Date', DateSchema);
+    return model;
 }
-
-
--- backend/user.js
+++ backend/user.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 
-module.exports = function(mongoose){
+module.exports = function (mongoose) {
     const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    mail: {
-        type: String,
-        required: true
-    }
-});
-const model = mongoose.model('User', UserSchema);
-return model;
+    const UserSchema = new Schema({
+        name: {
+            type: String,
+            required: true
+        },
+        mail: {
+            type: String,
+            required: true
+        }
+    });
+    const model = mongoose.model('User', UserSchema);
+    return model;
 }
