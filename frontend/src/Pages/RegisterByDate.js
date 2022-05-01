b1c059e08c6790be4c357ecef5c6175e763c90b8
ShirAmit96 <Shiramit96@gmail.com>
2022-05-01 11:42:33 +0300

    another css changes

--- frontend/src/Pages/RegisterByDate.js
+++ frontend/src/Pages/RegisterByDate.js
@@ -1,27 +1,42 @@
 import React from "react";
 import "./RegisterByDate.css";
-import Arrow from "./Arrow.svg";
-import { render } from "@testing-library/react";
-import { Link } from 'react-router-dom';
-// function goBack() {
-//  <link to="/.App.js"></link>;
-// }
+import Arrow from "../Icons/Arrow.svg";
+import { Link } from "react-router-dom";
+import postData from "../APIpost";
 
-function RegisterByDate () {
-  const registerToDate = () => {
-    
-  }
- 
- return (
+function RegisterByDate() {
+  //send id from the opening screen
+let date="18/4/22";
+let user="Shir";
+const setDate = () => {
+    postData("/assignToDate", { user,date })
+      .then((data) => {
+        console.log(data); // JSON data parsed by `data.json()` call
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+
+
+  return (
     <div className="RegisterByDate">
       <div className="upperArea">
+        <Link to="/OpeningScreen">
+          <img src={Arrow} className="Arrow" />
+        </Link>
         <h1 className="headline">When are you coming?</h1>
-       <Link to="/OpeningScreen"><img src={Arrow} className="Arrow" /></Link>
       </div>
-      <div className="howManyArea"><p className="attendance">See who registerd</p> </div>
       <div className="calander"></div>
-      <div className="lowerArea"><button className="continue">continue </button></div>
-     
+      <div className="howManyArea">
+        <p className="attendance">See who registerd</p>{" "}
+      </div>
+      <div className="lowerArea">
+        <button className="continue" onClick={setDate}>
+          continue{" "}
+        </button>
+      </div>
     </div>
   );
 }
