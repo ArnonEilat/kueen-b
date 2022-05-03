fd76b4e9a50492570aef93169bb2377c07dc7521
Liel Mushiev <lielmush@gmail.com>
2022-05-03 20:48:25 +0300

    merging with main

---
+++ frontend/src/components/calendar/calendarElement.js
@@ -0,0 +1,32 @@
+
+import { min } from 'moment';
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import './calendarElement.css';
+
+
+function MyCalendar() {
+    const [date, setDate] = useState(new Date());
+    var time = new Date();
+    var maxDate = time.setDate(time.getDate()+14);
+    var minDate = time.setDate(time.getDate()-14)
+    return (
+      <div className='myCal'>
+        <h1 className='text-center'><br></br></h1>
+        <div className='calendar-container'>
+          <Calendar onChange={setDate} value={date} next2Label={null} prev2Label={null} 
+          />
+        </div>
+        <p className='text center'>
+            <span className='bold'>Selected Date:</span>{' '}
+            {date.toDateString()}
+        </p>
+        <br></br>
+      </div>
+    );
+  }
+  
+
+
+  export default MyCalendar;
+
