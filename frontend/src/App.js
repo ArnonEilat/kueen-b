84858a4d4f8037e1edac9866cda076fb1ab73414
ShirAmit96 <Shiramit96@gmail.com>
2022-05-04 16:15:01 +0300

    removed commented code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,18 @@
 import React from "react";
 import OpeningScreen from "./Pages/OpeningScreen";
 import "./index.css";
-import { BrowserRouter,Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegisterByDate from "./Pages/RegisterByDate.js";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-    < Route path="/RegisterByDate" element={<RegisterByDate/>}/>
-       {/* < Route path="/RegisterByDate">
-          <RegisterByDate />
-        </Route> */}
-        < Route path="/OpeningScreen" element={<OpeningScreen/>}/>
-        < Route path="/" element={<OpeningScreen/>}/>
-          {/* <OpeningScreen/>
-        </Route> */}
-        {/* <Route path="/">
-          <OpeningScreen />
-        </Route> */}
-    </Routes>
+      <Routes>
+        <Route path="/RegisterByDate" element={<RegisterByDate />} />
+        <Route path="/OpeningScreen" element={<OpeningScreen />} />
+        <Route path="/" element={<OpeningScreen />} />
+      </Routes>
     </BrowserRouter>
-
   );
 }
 export default App;
