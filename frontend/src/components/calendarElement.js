e68e5b1929b7483a9cee6321df92b470c89284e9
Liel Mushiev <lielmush@gmail.com>
2022-05-10 16:31:09 +0300

    disable weekends

--- frontend/src/components/calendarElement.js
+++ frontend/src/components/calendarElement.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import './calendarElement.css';
+
+
+
+
 function MyCalendar() {
   const [date, setDate] = useState(new Date());
+  const headerRender = () => null;
   return (
     <div className='myCal'>
       <div className='calendar-container'>
-        <Calendar onChange={setDate} value={date} next2Label={null} prev2Label={null}   calendarType="Hebrew"
+        <Calendar onChange={setDate} value={date} next2Label={null} prev2Label={null} calendarType="Hebrew" onClickDecade={null} onClickMonth={null} onClickYear={null}
           navigationLabel={({ date, label, locale, view }) => {
             return date.toLocaleDateString('en-us', { month: 'long', year: 'numeric' });
           }
@@ -15,6 +20,11 @@ function MyCalendar() {
             return date.toLocaleDateString('en-us', { weekday: 'short' });
           }
           }
+          tileDisabled={({activeStartDate, date, view }) => {
+            return (date.getDay() === 6);
+          }
+          }
+          
         />
       </div>
     </div>
